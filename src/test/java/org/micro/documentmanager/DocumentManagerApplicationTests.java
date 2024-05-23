c744366146ad0f87a6e6d2758f50c1a16bd97537
BoshmanOvO <kumarashishbostan@gmail.com>
2024-05-23 16:23:05 +0530

    first commit

---
+++ src/test/java/org/micro/documentmanager/DocumentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.micro.documentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
